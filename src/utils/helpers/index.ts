09d6da0d615db86c7cd57ba2a26d4cc8d3db232a
Erik Henrique <ecunha@daitan.com>
2021-10-18 13:56:44 -0300

    remove unused debounce and also adds some minor changes

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,5 +1,4 @@
 export * from './convertHexToRGBA';
-export * from './debounce';
 export * from './getKilometersBetweenPoints';
 export * from './getLatitudeLongitudeFromText';
 export * from './hooks';
